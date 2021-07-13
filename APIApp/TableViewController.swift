2c9b5d07e58c9de841b68c5ed5402f4e12ec480f
duyphamvu47 <50992400+duyphamvu47@users.noreply.github.com>
2021-07-13 04:20:27 -0700

    Add loading indicator to TableView

--- APIApp/TableViewController.swift
+++ APIApp/TableViewController.swift
@@ -9,26 +9,77 @@ import UIKit
 
 class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet var tableView:UITableView!
+    let spinner = UIActivityIndicatorView(style: .medium)
+    
+    // dataSOurce
+    var data:[ApiResult] = []
+    
+    // variable for pagination
+    let limit: Int = 25
+    var currentIndex = 0        // Index of the finnal object getting from API
+    
+    // State when loading data
+    var noMoreData: Bool = false
+    var loadingData: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        spinner.frame = CGRect(x: 0.0, y: 0.0, width: tableView.bounds.width, height: 70)
+        tableView.tableFooterView = spinner
+        tableView.tableFooterView?.isHidden = false
+        spinner.startAnimating()
+        
+        loadDataUsingAPI()
     }
 
     
     //TableView func
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) 
-        cell.textLabel?.text = "I'm a label"
-        cell.detailTextLabel?.text = "You looking at the detail"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if !data.isEmpty{
+            cell.textLabel?.text = "Country: \(data[indexPath.row].country)"
+            cell.detailTextLabel?.text = "(\(data[indexPath.row].latitude), \(data[indexPath.row].longtitude))"
+        }
         return cell
     }
-
-
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let lastElement = data.count - 1
+        
+        // reaching last element
+        if !loadingData && indexPath.row == lastElement && !noMoreData{
+            loadingData = true
+            // run loading indicator
+            spinner.startAnimating()
+            tableView.tableHeaderView?.isHidden = false
+            loadDataUsingAPI()
+        }
+    }
+    
+    func loadDataUsingAPI(){
+        APIHelper.getData(limit: limit, offset: currentIndex, completion: { [self] result in
+            // loading final object from API
+            if(result.count < limit){
+                noMoreData = true
+                currentIndex += result.count
+            }else{
+                currentIndex += limit
+            }
+            
+            data.append(contentsOf: result)
+            tableView.reloadData()
+            loadingData = false
+            tableView.tableFooterView?.isHidden = true
+            spinner.stopAnimating()
+        })
+    }
 
 }
